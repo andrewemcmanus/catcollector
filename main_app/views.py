abda296175bf1a8e992a9ab267c28ad483ea378c
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-01-14 15:49:22 -0500

    add form

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Cat
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponse, HttpResponseRedirect
 # class Cat:
 #     def __init__(self, name, breed, description, age):
 #         self.name = name
@@ -24,3 +25,24 @@ def about(request):
 def cats_index(request):
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+def cats_show(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/show.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = '/cats'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        return HttpResponseRedirect('/cats')
+
+class CatUpdate(DeleteView):
+    model = Cat
+    success_url = '/cats'
